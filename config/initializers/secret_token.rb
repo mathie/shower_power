294bc7796086c74364697af3480ef68bb9d9fe0d
Graeme Mathieson <mathie@woss.name>
2012-03-01 15:48:00 +0000

    rails new shower_power -T -O

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ShowerPower::Application.config.secret_token = '71a869e2f2048381921133b77b55a22e0f2e9a9ddcbd9f92d6bd48f8ca0784bb3e72443f36ed9b3a4013b132da99f91915aceb791c4f9623955583856890bec0'

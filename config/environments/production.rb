a38685c35a3b1c30e3745aa1accd22f3407da7e3
Graeme Mathieson <mathie@woss.name>
2012-04-21 09:36:43 +0100

    Use Cloudfront for asset distribution.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,7 +43,7 @@ ShowerPower::Application.configure do
   # config.cache_store = :mem_cache_store
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
-  config.action_controller.asset_host = "http://#{ENV['FOG_DIRECTORY']}.s3.amazonaws.com"
+  config.action_controller.asset_host = "http://d2hoo6otf466n7.cloudfront.net"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )

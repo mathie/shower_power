71b7ccec3ae3a1adaa8e15885bec138ac6849c33
Graeme Mathieson <mathie@woss.name>
2012-04-22 15:46:19 +0100

    Don't show the appcache in development, it'll only hurt.

--- app/controllers/appcache_controller.rb
+++ app/controllers/appcache_controller.rb
@@ -2,5 +2,6 @@ class AppcacheController < ApplicationController
   layout false
 
   def index
+    render status: 404 if Rails.env.development?
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 ShowerPower::Application.routes.draw do
-  match '/application.manifest', to: 'appcache#index', format: :appcache
+  match '/application.manifest', to: 'appcache#index', format: :appcache, as: :manifest
   match '/about', to: 'pages#about'
   root to: 'pages#index'
 end
